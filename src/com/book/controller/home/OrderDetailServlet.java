6673c72a60fbec2b430213df7bc94685c19aeeb2
yostar <yoovanchan@163.com>
2019-06-04 19:25:55 +0800

    添加前台servlet进行页面跳转

---
+++ src/com/book/controller/home/OrderDetailServlet.java
@@ -0,0 +1,21 @@
+package com.book.controller.home;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "OrderDetailServlet")
+public class OrderDetailServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/home/orderDetail.jsp");
+        dispatcher.forward(request, response);
+    }
+}
